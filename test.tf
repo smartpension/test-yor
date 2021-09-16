6f5f9fa3acf747bc1685b47e5831b5bff7193e98
Leon Allen <leon.allen@smartretire.com>
2021-09-16 14:16:41 -0500

    test

--- test.tf
+++ test.tf
@@ -58,3 +58,26 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 }
+
+resource "aws_instance" "test_instance3" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  root_block_device {
+    encrypted = true
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+  tags = {
+    git_commit           = "d5010d930d5e5a0561f7de4d3a7fd7aea330f85a"
+    git_file             = "test.tf"
+    git_last_modified_at = "2021-08-27 19:55:31"
+    git_last_modified_by = "leon.allen@smartretire.com"
+    git_modifiers        = "leon.allen"
+    git_org              = "smartpension"
+    git_repo             = "test-yor"
+    yor_trace            = "da0e308e-35c1-4c93-b57e-798997ce8395"
+  }
+}
